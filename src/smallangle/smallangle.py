bea7c15d4e4a64286b00decccfff132f965cf8df
Rinse Postuma <rinse.postuma@student.uva.nl>
2024-11-14 12:37:50 +0100

    Eerste versie

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -3,18 +3,33 @@ import numpy as np
 from numpy import pi
 import pandas as pd
 
-
+@click.group()
+def smallangle():
+    """ A tool to calculate the outcomes of sinus and tangent between 0 and 2 pi """
+    pass
+@smallangle.command()
+@click.option("-n", "--number", default=50, show_default=True, help="Number of steps between 0 and 2 pi")
 def sin(number):
+    """Generate a list of x and sin(x) between 0 and 2 pi
+
+    Args:
+        number (int): number of steps between 0 and 2 pi
+    """    
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
 
-
+@smallangle.command()
+@click.option("-n", "--number", default=50, show_default=True, help="Number of steps between 0 and 2 pi")
 def tan(number):
+    """Generate a list of x and sin(x) between 0 and 2 pi
+
+    Args:
+        number (int): number of steps between 0 and 2 pi
+    """
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
 
-
 if __name__ == "__main__":
-    sin(10)
+    smallangle()
